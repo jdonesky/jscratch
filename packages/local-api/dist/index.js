ef18080e5f079b392dc482c2804897300e08e0ec
Jon <jdonesky@gmail.com>
2021-02-06 09:41:18 -0500

    base commit after rebuild following npm breaking lerna symlinks

---
+++ packages/local-api/dist/index.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.serve = void 0;
+var express_1 = __importDefault(require("express"));
+var serve = function (port, filename, dir) {
+    var app = express_1.default();
+    app.use(express_1.default.static('../../local-client/build'));
+    // app.use(createProxyMiddleware({
+    //     target: 'http://localhost:3000',
+    //     ws: true,
+    //     logLevel: 'silent'
+    // }))
+    return new Promise(function (resolve, reject) {
+        app.listen(port, resolve).on('error', reject);
+    });
+};
+exports.serve = serve;
